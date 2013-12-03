5617ae3b3c626fe8c23a37175b0132120f50bb3d
idontdomath <ramosmejiamartin@gmail.com>
2013-12-03 10:44:18 -0300

    - added nginx, upstart and monit to the package

--- manifests/base-nodejs.pp
+++ manifests/base-nodejs.pp
@@ -4,3 +4,14 @@ group { "puppet":
   ensure => "present",
 }
 
+exec { 'install upstart':
+  command => '/usr/bin/apt-get install -y upstart',
+}
+
+exec { 'install monit':
+  command => '/usr/bin/apt-get install -y monit',
+}
+
+exec { 'install nginx':
+  command => '/usr/bin/apt-get install -y nginx',
+}
