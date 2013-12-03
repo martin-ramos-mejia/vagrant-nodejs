d24788c280789c94aec4956eb0027a0ca88f4a4d
idontdomath <ramosmejiamartin@gmail.com>
2013-12-03 16:32:20 -0300

    - added configuration for upstart - added configuration for monit - general changes to automate the creation of the environment

--- manifests/base-nodejs.pp
+++ manifests/base-nodejs.pp
@@ -16,7 +16,7 @@ exec { 'install nginx':
   command => '/usr/bin/apt-get install -y nginx',
 }
 
-#configuration of nginx
+# Configuration of nginx
 file { "/etc/nginx/sites-available/default":
   source => "puppet:///modules/nodejs-conf/nginx-default",
   mode => 644,
@@ -25,11 +25,30 @@ file { "/etc/nginx/sites-available/default":
   require => Exec['apt-get update']
 }
 
-#demo app
-file { "/home/vagrant/test.js":
+# creation of demo app
+# TODO: better path ;)
+file { "/root/test.js":
   source => "puppet:///modules/nodejs-conf/test.js",
   mode => 644,
-  owner => vagrant,
-  group => vagrant,
+  owner => root,
+  group => root,
+  require => Exec['apt-get update']
+}
+
+# creation of upstart configuration for test.js
+file { "/etc/init/test.conf":
+  source => "puppet:///modules/nodejs-conf/test.conf",
+  mode => 744,
+  owner => root,
+  group => root,
+  require => Exec['apt-get update']
+}
+
+# creation of monit for test.js
+file { "/etc/monit/monitrc":
+  source => "puppet:///modules/nodejs-conf/monitrc",
+  mode => 700,
+  owner => root,
+  group => root,
   require => Exec['apt-get update']
 }
